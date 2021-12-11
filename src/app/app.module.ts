bcdb917c3713b967ee142ac93aa118a4a9f8d878
Yuuri Gomes <contato.yuricgomes@gmail.com>
2021-12-11 13:58:30 -0300

    poke list - hard code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { PokeListComponent } from './components/poke-list/poke-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PokeListComponent
   ],
   imports: [
     BrowserModule,
